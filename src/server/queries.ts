8a4f519614471d2f0b4abc24c92be09cedc52dd9
grab777 <grab777@gmail.com>
2024-12-25 17:43:05 +0100

    add missing files

---
+++ src/server/queries.ts
@@ -0,0 +1,16 @@
+import "server-only";
+import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+
+export const getMyImages = async () => {
+  const user = await auth();
+  if (!user.userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.uploadedBy, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return images;
+};
